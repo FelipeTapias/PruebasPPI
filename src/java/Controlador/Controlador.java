ed13d201425c548df9b83faa40eae17e43e14ea9
Felipe <tapiastuberquia@gmail.com>
2020-06-05 01:30:57 -0500

    Se sube proyecto para pruebas

---
+++ src/java/Controlador/Controlador.java
@@ -0,0 +1,93 @@
+
+package Controlador;
+
+import Modelo.Usuario;
+import Modelo.UsuarioDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Controlador extends HttpServlet {
+    
+    UsuarioDAO dao = new UsuarioDAO();
+    Usuario us = new Usuario();
+ 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Controlador</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+        switch (accion) {
+            case "Listar":
+                List<Usuario>datos = dao.listar();
+                request.setAttribute("datos", datos);
+                request.getRequestDispatcher("RegistrosUsu.jsp").forward(request, response);
+                break;
+            case "Nuevo":
+                request.getRequestDispatcher("registrar.jsp").forward(request, response);
+                break;
+            case "Registrar":
+                String id = request.getParameter("txtid");
+                String nombre = request.getParameter("txtnombre");
+                String apellido = request.getParameter("txtapellido");
+                String correo = request.getParameter("txtcorreo");
+                String cargo = request.getParameter("txtcargo");
+                String contraseña = request.getParameter("txtcontrasena");
+                String direccion = request.getParameter("txtdireccion");
+                us.setId(id);
+                us.setNombre(nombre);
+                us.setApellido(apellido);
+                us.setCorreo(correo);
+                us.setCargo(cargo);
+                us.setContraseña(contraseña);
+                us.setDireccion(direccion);
+                dao.agregar(us);
+                request.getRequestDispatcher("Controlador?accion=Listar").forward(request, response);
+                break;
+            case "Editar":
+                String ide = request.getParameter("id");
+                Usuario us = dao.listarId(ide);
+                request.setAttribute("usuario", us);
+                request.getRequestDispatcher("editarUsu.jsp").forward(request, response);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+     
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/Modelo/UsuarioDAO.java
@@ -0,0 +1,106 @@
+package Modelo;
+
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+public class UsuarioDAO {
+    PreparedStatement ps;
+    ResultSet rs;
+    Conexion c = new Conexion();
+    Connection con;
+    
+    public List listar(){
+       List<Usuario>lista = new ArrayList<>();
+       String sql = "select * from USUARIO";
+        try {
+            con = c.conectar();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {                
+               Usuario us = new Usuario();
+               us.setId(rs.getString(1));
+               us.setNombre(rs.getString(2));
+               us.setApellido(rs.getString(3));
+               us.setCorreo(rs.getString(4));
+               us.setCargo(rs.getString(5));
+               us.setContraseña(rs.getString(6));
+               us.setDireccion(rs.getString(7));
+               lista.add(us);
+            }
+        } catch (Exception e) {
+        }
+        return lista;
+    }
+    public int agregar(Usuario us) {
+     int r=0;
+     String sql="insert into usuario(id,nombre,apellido,correo,cargo,contraseña,direccion) values(?,?,?,?,?,?,?)";
+        try {
+           con = c.conectar();
+           ps = con.prepareStatement(sql);
+           ps.setString(1, us.getId());
+           ps.setString(2, us.getNombre());
+           ps.setString(3, us.getApellido());
+           ps.setString(4, us.getCorreo());
+           ps.setString(5, us.getCargo());
+           ps.setString(6, us.getContraseña());
+           ps.setString(7, us.getDireccion());
+           r = ps.executeUpdate();
+            if (r == 1) {
+                r = 1;
+            }else {
+                r = 0;
+            }
+        } catch (Exception e) {
+        }
+        return r;
+    }
+    
+   public Usuario listarId(String id){
+       String sql = "select * from usuario where id="+id;
+       Usuario us = new Usuario();
+       try {
+           con = c.conectar();
+           ps = con.prepareStatement(sql);
+           rs = ps.executeQuery();
+           while (rs.next()) {
+               us.setId(rs.getString(1));
+               us.setNombre(rs.getString(2));
+               us.setApellido(rs.getString(3));
+               us.setCorreo(rs.getString(4));
+               us.setCargo(rs.getString(5));
+               us.setContraseña(rs.getString(6));
+               us.setDireccion(rs.getString(7));
+           }
+       } catch (Exception e) {
+       }
+       return us;
+   }
+   public int actualizar(Usuario us){
+       int r = 0;
+       String sql = "update usuario set nombre=?,apellido=?,correo=?,cargo=?,contraseña=?,direccion=?"
+                  + " where id=?";
+       try {
+           con = c.conectar();
+           ps = con.prepareStatement(sql);
+           ps.setString(1, (us.getNombre()));
+           ps.setString(2, (us.getApellido()));
+           ps.setString(3, (us.getCorreo()));
+           ps.setString(4, (us.getCargo()));
+           ps.setString(5, (us.getContraseña()));
+           ps.setString(6, (us.getDireccion()));
+           ps.setString(7, (us.getId()));
+           r = ps.executeUpdate();
+           if (r == 1) {
+               r = 1;
+           }else{
+               r = 0;
+           }
+       } catch (Exception e) {
+       }
+       return r;
+   }
+}
